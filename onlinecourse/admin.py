78acb3deaa51068ce0f31d5f7ef8c26cf89fddee
so-SKD <sarah@stellakinesisdesign.com>
2026-01-06 12:47:13 -0500

    Register Question, Choice, and Submission in admin site

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -28,3 +38,7 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
+
